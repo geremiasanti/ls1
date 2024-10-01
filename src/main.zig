e477093275e72809731d956147c087143f2df2f3
geremia <germysettesette@gmail.com>
2024-10-01 19:42:36 +0200

    command line arg + cwd

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,16 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var args = std.process.args();
+    _ = args.skip();
+    const first_arg = args.next();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const cwd = try std.fs.cwd().realpathAlloc(alloc, ".");
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    std.log.info("cwd: {s}", .{cwd});
+    std.log.info("first_arg: {?s}", .{first_arg});
 }
