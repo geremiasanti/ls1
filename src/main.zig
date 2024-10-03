d75d89613d299679afeafa287ed37eb3a5d58e31
geremia <germysettesette@gmail.com>
2024-10-03 16:55:10 +0200

    is path file or directory?

--- src/main.zig
+++ src/main.zig
@@ -2,26 +2,33 @@ const std = @import("std");
 
 pub fn main() !void {
     // allocator
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const alloc = arena.allocator();
+    //var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    //defer arena.deinit();
+    //const alloc = arena.allocator();
 
     // stout writer
-    const stdout_writer = std.io.getStdOut().writer();
+    const stout_writer = std.io.getStdOut().writer();
 
     // get first argument
     var args = std.process.args();
     _ = args.skip();
     const dir_path = args.next() orelse ".";
 
-    // get dir absolute path
-    const dir_absolute_path = std.fs.cwd().realpathAlloc(alloc, dir_path) catch |err| switch (err) {
-        std.fs.Dir.RealPathAllocError.FileNotFound => {
-            try stdout_writer.print("cannot access '{s}': No such file or directory\n", .{dir_path});
+    // is path a folder or a file?
+    const stat = std.fs.cwd().statFile(dir_path) catch |err| switch (err) {
+        std.fs.Dir.StatFileError.FileNotFound => {
+            try stout_writer.print("cannot access '{s}': No such file or directory\n", .{dir_path});
             return;
         },
         else => return err,
     };
+    switch (stat.kind) {
+        .directory => std.debug.print("dir!\n", .{}),
+        .file => std.debug.print("file!\n", .{}),
+        else => {
+            try stout_writer.print("Weird {s} shit, not currently supported\n", .{@tagName(stat.kind)});
+        },
+    }
 
-    std.log.info("{s}", .{dir_absolute_path});
+    //std.log.info("{s}", .{dir_absolute_path});
 }
