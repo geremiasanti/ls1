87f2cd19e63aeb2d2f3afabe472946bdc0e054b6
geremia <germysettesette@gmail.com>
2024-10-03 14:08:31 +0200

    handling file not found error;

--- src/main.zig
+++ src/main.zig
@@ -5,14 +5,19 @@ pub fn main() !void {
     defer arena.deinit();
     const alloc = arena.allocator();
 
-    // get args and skip command
+    // get first argument
     var args = std.process.args();
     _ = args.skip();
+    const dir_path = args.next() orelse ".";
 
-    const dir_absolute_path = try std.fs.cwd().realpathAlloc(
-        alloc,
-        args.next() orelse ".",
-    );
+    // get dir absolute path
+    const dir_absolute_path = std.fs.cwd().realpathAlloc(alloc, dir_path) catch |err| switch (err) {
+        std.fs.Dir.RealPathAllocError.FileNotFound => {
+            std.log.info("cannot access '{s}': No such file or directory", .{dir_path});
+            return;
+        },
+        else => unreachable,
+    };
 
     std.log.info("{s}", .{dir_absolute_path});
 }
