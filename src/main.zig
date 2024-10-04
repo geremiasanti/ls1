853cfdbc6831bab4b0139d2b7de5c84cd824d4e9
geremia <germysettesette@gmail.com>
2024-10-04 12:26:36 +0200

    functions return output instead of printing

--- src/main.zig
+++ src/main.zig
@@ -7,45 +7,71 @@ pub fn main() !void {
     _ = args.skip();
     const path = args.next() orelse ".";
 
-    try ls1(path);
+    // program
+    const out = try ls1(path);
+
+    // stout writer
+    const stout_writer = std.io.getStdOut().writer();
+    try stout_writer.print("{s}", .{out});
 }
 
-fn ls1(path: []const u8) !void {
+fn ls1(path: []const u8) ![]u8 {
     // allocator
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const alloc = arena.allocator();
 
-    // stout writer
-    const stout_writer = std.io.getStdOut().writer();
-
     // get stat file of path
     const cwd = std.fs.cwd();
     const stat = cwd.statFile(path) catch |err| switch (err) {
-        std.fs.Dir.StatFileError.FileNotFound => {
-            try stout_writer.print("cannot access '{s}': No such file or directory\n", .{path});
-            return;
-        },
+        std.fs.Dir.StatFileError.FileNotFound => return try std.fmt.allocPrint(
+            alloc,
+            "cannot access '{s}': No such file or directory\n",
+            .{path},
+        ),
         else => return err,
     };
 
     // handle different kinds
     switch (stat.kind) {
         // TODO: close dir and file
-        .directory => try showDir(alloc, try cwd.openDir(path, .{})),
-        .file => showFile(std.fs.path.basename(path), try cwd.openFile(path, .{})),
+        .directory => {
+            var dir = try cwd.openDir(path, .{});
+            defer dir.close();
+
+            return try showDir(alloc, dir);
+        },
+        .file => {
+            var file = try cwd.openFile(path, .{});
+            defer file.close();
+
+            return try showFile(alloc, std.fs.path.basename(path), file);
+        },
         else => {
-            try stout_writer.print("Weird {s} shit, not currently supported\n", .{@tagName(stat.kind)});
+            return try std.fmt.allocPrint(
+                alloc,
+                "Weird {s} shit, not currently supported\n",
+                .{@tagName(stat.kind)},
+            );
         },
     }
+
+    return try std.fmt.allocPrint(alloc, "\n", .{});
 }
 
-// TODO: make this return text
-fn showDir(alloc: Allocator, dir: std.fs.Dir) std.fs.Dir.RealPathAllocError!void {
-    std.log.info("{s}", .{try dir.realpathAlloc(alloc, ".")});
+const ShowDirError = std.fs.Dir.RealPathAllocError || std.fmt.AllocPrintError;
+fn showDir(alloc: Allocator, dir: std.fs.Dir) ShowDirError![]u8 {
+    return try std.fmt.allocPrint(
+        alloc,
+        "dir: {s}'\n",
+        .{try dir.realpathAlloc(alloc, ".")},
+    );
 }
 
-// TODO: make this return text
-fn showFile(basename: []const u8, _: std.fs.File) void {
-    std.log.info("{s}", .{basename});
+fn showFile(alloc: Allocator, basename: []const u8, _: std.fs.File) std.fmt.AllocPrintError![]u8 {
+    return try std.fmt.allocPrint(
+        alloc,
+        "file: {s}'\n",
+        .{basename},
+    );
 }
